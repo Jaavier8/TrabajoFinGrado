27036a8554e9c3604e31ed397ce4cf9c45acbe56
Jaavier8 <javierrodriguez987@gmail.com>
2021-06-17 20:24:01 -0400

    feat: SPIN rules created

--- tfg/otx_alienvault/generate_rules.py
+++ tfg/otx_alienvault/generate_rules.py
@@ -1,5 +1,6 @@
 import subprocess
 import sys, os
+import json
 
 ################################
 
@@ -31,71 +32,107 @@ def cmd(cmd):
 cve = ["CVE-2021-26855", "CVE-2017-8570", "CVE-2021-26857", "CVE-2021-26858", "CVE-2019-11510",
         "CVE-2021-27065", "CVE-2021-27075", "CVE-2017-1000253", "CVE-2020-1472", "CVE-2017-0147"]
 
-for item in cve:
-    cmd(f"curl http://cve.circl.lu/api/cve/{item} >> test_cve/test_{item}.json")
+# for item in cve:
+#     cmd(f"curl http://cve.circl.lu/api/cve/{item} >> test_cve/test_{item}.json")
+# cve, campaignId, malwareName
+def generate_rules(cvename, campaignid, malwarename):
+    cmd(f"curl http://cve.circl.lu/api/cve/{cvename} >> cve.json")
+    with open('cve.json') as file:
+        data = json.load(file)
+    cvesummary = data["summary"]
+    cvss = data["cvss"]
+    attackpatterns = data["capec"]
+    cmd("rm cve.json")
+    generate_vulnerability(cvesummary, cvename, campaignid, malwarename)
+    generate_threat(cvss, cvename)
+    if(len(attackpatterns) >= 0):
+        generate_attack_pattern(attackpatterns, cvename, malwarename)
+        generate_course_action(attackpatterns, cvename, malwarename)
+    # with open("otx_alienvault/SPINrules_templates/threat.txt") as f:
+    #     print()
+        # lineas = f.readlines();
+        # for linea in lineas:
+        #     print(linea)
+        #     if("cvss" in linea):
+        #         line = linea.replace('cvss','hola')
+        #         print(line)
 
-def generate_rules(cve):
-"""
-// Regla para generar una vulnerabilidad y asociarla a Campaign y Malware
-CONSTRUCT {
-	?newv a cyberthreat_STIX:Vulnerability.
-	?newv cyberthreat_STIX:description {summary} .
-	?newv cibersituational-ontology:name {name} .
-	?newv cyberthreat_STIX:isTargetedBy ?c.
-    ?c cyberthreat_STIX:targets ?newv.
-	?newv cyberthreat_STIX:isTargetedBy ?m.
-    ?m cyberthreat_STIX:targets ?newv.
-}
-WHERE{
-	?c a cyberthreat_STIX:Campaign.
-	?m a cyberthreat_STIX:Malware.
-    ?c cyberthreat_STIX:id {id}.
-    ?m cibersituational-ontology:name {mname}.
-}
+def generate_vulnerability(cvesummary, cvename, campaignid, malwarename):
+    rule = f"""CONSTRUCT{{
+    	?newv a cyberthreat_STIX:Vulnerability.
+    	?newv cyberthreat_STIX:description {cvesummary} .
+    	?newv cibersituational-ontology:name {cvename} .
+    	?newv cyberthreat_STIX:isTargetedBy ?c.
+        ?c cyberthreat_STIX:targets ?newv.
+    	?newv cyberthreat_STIX:isTargetedBy ?m.
+        ?m cyberthreat_STIX:targets ?newv.
+    }}
+    WHERE{{
+    	?c a cyberthreat_STIX:Campaign.
+    	?m a cyberthreat_STIX:Malware.
+        ?c cyberthreat_STIX:id {campaignid}.
+        ?m cibersituational-ontology:name {malwarename}.
+    }}"""
+    with open(f"otx_alienvault/SPINrules/{cvename}_vulnerability", "w") as f:
+        f.write(rule)
 
-// Regla para generar una amenaza a través de la vulnerabilidad anterior
-CONSTRUCT {
-	?newa a cyberthreat_DRM:Threat.
-	?newa cibersituational-ontology:impact {cvss}^^xsd:float .
-	?newa cyberthreat_STIX:exploits ?v.
-}
-WHERE{
-	?v a cyberthreat_STIX:Vulnerability.
-	?v cibersituational-ontology:name {name}.
-}
+def generate_threat(cvss, cvename):
+    rule = f"""CONSTRUCT{{
+    	?newa a cyberthreat_DRM:Threat.
+    	?newa cibersituational-ontology:impact {cvss}^^xsd:float .
+    	?newa cyberthreat_STIX:exploits ?v.
+    }}
+    WHERE{{
+    	?v a cyberthreat_STIX:Vulnerability.
+    	?v cibersituational-ontology:name {cvename}.
+    }}"""
+    with open(f"otx_alienvault/SPINrules/{cvename}_threat", "w") as f:
+        f.write(rule)
 
-// Regla para generar un Attack Pattern de la vulnerabilidad anterior
-CONSTRUCT {
-	?newap a cyberthreat_STIX:Attack_Pattern.
-    ?newap cibersituational-ontology:name {attackname}.
-    ?newap cyberthreat_STIX:description {attackdescription}.
-    ?newap cyberthreat_STIX:uses ?m.
-    ?m cyberthreat_STIX:isUsedBy ?newap
-	?newap cyberthreat_STIX:targets ?v .
-}
-WHERE{
-	?v a cyberthreat_STIX:Vulnerability.
-    ?m a cyberthreat_STIX:Malware.
-	?v cibersituational-ontology:name {name}.
-    ?m cibersituational-ontology:name {mname}.
-}
+def generate_attack_pattern(attackpatterns, cvename, malwarename):
+    i = 0
+    for at in attackpatterns:
+        rule = f"""CONSTRUCT{{
+        	?newap a cyberthreat_STIX:Attack_Pattern.
+            ?newap cibersituational-ontology:name {at["name"]}.
+            ?newap cyberthreat_STIX:description {at["summary"]}.
+            ?newap cyberthreat_STIX:uses ?m.
+            ?m cyberthreat_STIX:isUsedBy ?newap
+        	?newap cyberthreat_STIX:targets ?v .
+        }}
+        WHERE{{
+        	?v a cyberthreat_STIX:Vulnerability.
+            ?m a cyberthreat_STIX:Malware.
+        	?v cibersituational-ontology:name {cvename}.
+            ?m cibersituational-ontology:name {malwarename}.
+        }}"""
+        with open(f"otx_alienvault/SPINrules/{cvename}_attackpattern_{i}", "w") as f:
+            f.write(rule)
+        i = i+1
 
-// Regla para generar un Course of Action para el Attack Pattern anterior
-CONSTRUCT {
-	?newca a cyberthreat_STIX:Course_of_Action.
-	?newca cyberthreat_STIX:description {solutions} .
-    ?newca cyberthreat_DRM:mitigates ?ap.
-    ?newca cyberthreat_DRM:mitigates ?m.
-    ?newca cyberthreat_STIX:remediates ?v.
-    ?newca cyberthreat_STIX:remediates ?m.
-    ?m cibersituational-ontology:isMitigatedBy ?newca.
-}
-WHERE{
-	?v a cyberthreat_STIX:Vulnerability.
-    ?m a cyberthreat_STIX:Malware.
-    ?ap a cyberthreat_STIX:Attack_Pattern.
-	?v cibersituational-ontology:name {name}.
-    ?m cibersituational-ontology:name {mname}.
-    ?m cibersituational-ontology:name {attackname}.
-}
-"""
+def generate_course_action(attackpatterns, cvename, malwarename):
+    i = 0
+    for at in attackpatterns:
+        rule = f"""CONSTRUCT{{
+        	?newca a cyberthreat_STIX:Course_of_Action.
+        	?newca cyberthreat_STIX:description {at["solutions"]} .
+            ?newca cyberthreat_DRM:mitigates ?ap.
+            ?newca cyberthreat_DRM:mitigates ?m.
+            ?newca cyberthreat_STIX:remediates ?v.
+            ?newca cyberthreat_STIX:remediates ?m.
+            ?m cibersituational-ontology:isMitigatedBy ?newca.
+        }}
+        WHERE{{
+        	?v a cyberthreat_STIX:Vulnerability.
+            ?m a cyberthreat_STIX:Malware.
+            ?ap a cyberthreat_STIX:Attack_Pattern.
+        	?v cibersituational-ontology:name {cvename}.
+            ?m cibersituational-ontology:name {malwarename}.
+            ?m cibersituational-ontology:name {at["name"]}.
+        }}"""
+        with open(f"otx_alienvault/SPINrules/{cvename}_courseofaction_{i}", "w") as f:
+            f.write(rule)
+        i = i+1
+
+
+generate_rules("CVE-2017-0147", "campaign--b1509899-4f95-4b1e-8fd5-76cbd80baf9a", "Matanbuchus")
